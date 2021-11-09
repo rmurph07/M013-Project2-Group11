93d3677af30eba8872222f634a83e0ae47ed353f
HonestVervaine <92113014+HonestVervaine@users.noreply.github.com>
2021-11-09 20:37:50 +0400

    Create explotOnly.py
    Start of exploitonly.py (?)

---
+++ explotOnly.py
@@ -0,0 +1,12 @@
+Y'all, I'm gonna need some help w this one lmao. Although the idea is pretty simple. I'm not in ECS102 (or whatever it is that teaches u python) cuz im in cis252 (learning haskell) 
+so i ain't got no clue how to start this shit.
+
+Got a few questions and I should be able to get the ball rolling, lmk when you guys have time. 
+
+I've been programming for personal (non school) stuff and summer camps for like 6 years now in c# java and ++ so i think i can figure it out if you guys don't end up having time
+but agian, I know no python so it'll j take longer due to figuring out the basic syntax n shit.
+
+last project I was doing the binary to char translation (we called decoding?) and it was j basically a series of a ton of if statements so shit was easy to j keep repeating
+but this an actual function and IK Ryan needs it to work for his part to work so I ain't trying to mess it up.
+
+~ Alex (ExploitOnly).
