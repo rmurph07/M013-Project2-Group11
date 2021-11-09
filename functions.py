e1114d4a610a9dbcd628497739dc1de51f5e87a5
almirchowdhury <aachowdh@syr.edu>
2021-11-09 12:17:59 -0500

    functions.py

---
+++ functions.py
@@ -0,0 +1,16 @@
+import random
+
+c1 = random.normalvariate(9, 3)
+c2 = random.normalvariate(7, 5)
+c3 = random.normalvariate(11, 7)
+
+def exploreOnly() -> int:
+    result = 0
+    for i in range(100):
+        result += c1
+        result += c2
+        result += c3
+    print(result)
+    return result
+
+
